941a9b3f4abf8563342ca315c4c2499d6a7a30ed
Gary Zhao <gary.j.zhao@gmail.com>
2018-12-12 20:17:02 -0800

    Refactored App and added Cornerstone.js

---
+++ src/components/CornerstoneElement.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+import * as cornerstone from "cornerstone-core";
+import * as cornerstoneMath from "cornerstone-math";
+import * as cornerstoneTools from "cornerstone-tools";
+import * as cornerstoneWebImageLoader from "cornerstone-web-image-loader";
+
+cornerstoneTools.external.cornerstone = cornerstone;
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
+cornerstoneWebImageLoader.external.cornerstone = cornerstone;
+
+const imageId = "../images/test.jpg";
