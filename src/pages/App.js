ff84a16d85e7bc96f772af0623a3348b68dc9a44
Gary Zhao <gary.j.zhao@gmail.com>
2018-12-12 20:05:59 -0800

    dev

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import "../css/App.css";
-
+import styled from "styled-components";
 import Layout from "../components/Layout";
+
+const Wrapper = styled.div`
+  max-width: 1000px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 16px;
+  box-sizing: border-box;
+`;
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <h1>Hello World -- App</h1>
+        <Wrapper>
+          <h1>Hello World</h1>
+        </Wrapper>
       </Layout>
     );
   }
