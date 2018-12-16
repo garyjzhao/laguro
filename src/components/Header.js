5ff76d402d64e836925f4a215e90aa33d2b15db2
Gary Zhao <gary.j.zhao@gmail.com>
2018-12-16 22:52:22 +0900

    added logo and restyled header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,69 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { ReactComponent as Logo } from "../images/logo.svg";
+
+const FullDarkBg = styled.div`
+  background-color: var(--dark-bg);
+`;
 
 const Wrapper = styled.div`
   max-width: 1000px;
   width: 100%;
   margin: 0 auto;
-  padding: 0 16px;
+  padding: 16px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   box-sizing: border-box;
+  color: #fff;
 
   a {
     text-decoration: none;
     font-size: 1em;
     font-weight: bold;
-    color: #000;
+    color: #fff;
     padding-bottom: 2px;
     margin-right: 15px;
     &:hover {
-      border-bottom: 2px solid #000;
+      border-bottom: 2px solid #fff;
       padding-bottom: 0;
     }
+    &#logo:hover {
+      border-bottom: 0;
+      padding-bottom: 2px;
+    }
+  }
+`;
+
+const LogoWrapper = styled.div`
+  max-width: 88px;
+  svg {
+    fill: black;
   }
 `;
 
 class Header extends Component {
   render() {
     return (
-      <Wrapper>
-        <h1>Laguro</h1>
-        {this.props.isLoggedIn ? (
-          <div>
-            <a href="/App">My X-Rays</a>
-            <a href="/" onClick={this.props.logout}>
-              Log Off
-            </a>
-          </div>
-        ) : (
-          <a href="/">Login</a>
-        )}
-      </Wrapper>
+      <FullDarkBg>
+        <Wrapper>
+          <a href="/App" id="logo">
+            <LogoWrapper>
+              <Logo />
+            </LogoWrapper>
+          </a>
+          {this.props.isLoggedIn ? (
+            <div>
+              <a href="/App">My X-Rays</a>
+              <a href="/" onClick={this.props.logout}>
+                Log Off
+              </a>
+            </div>
+          ) : (
+            <a href="/">Login</a>
+          )}
+        </Wrapper>
+      </FullDarkBg>
     );
   }
 }
