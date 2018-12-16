26251a18ff3020a53c9c5fcf47e0d69e12187101
Gary Zhao <gary.j.zhao@gmail.com>
2018-12-17 00:18:35 +0900

    Updates

---
+++ precache-manifest.8ec59da8774bbf29d127b77af8d713e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a17f435c6e290e2f781f",
+    "url": "/laguro/static/css/main.ca6e5ba6.chunk.css"
+  },
+  {
+    "revision": "a17f435c6e290e2f781f",
+    "url": "/laguro/static/js/main.a17f435c.chunk.js"
+  },
+  {
+    "revision": "b71592dbcd4b593d1ec9",
+    "url": "/laguro/static/js/1.b71592db.chunk.js"
+  },
+  {
+    "revision": "ea8c82efc7d7bbf52d95",
+    "url": "/laguro/static/js/runtime~main.ea8c82ef.js"
+  },
+  {
+    "revision": "9641aa45a0de94b32824421cd492b20d",
+    "url": "/laguro/static/media/logo.9641aa45.svg"
+  },
+  {
+    "revision": "bdc3a33effc9dfe44d07662f43ee83d6",
+    "url": "/laguro/index.html"
+  }
+];
